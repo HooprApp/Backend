20d96c9462d540f736e7e873ed2d1dd1d0b0ba4a
hoklaamc <hoklaam.cheng@gmail.com>
2020-09-22 00:59:38 -0400

    Remove code dependent on firebase_database

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hoopr/services/authentication.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:hoopr/models/todo.dart';
 import 'dart:async';
 
 class HomePage extends StatefulWidget {
@@ -17,17 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Todo> _todoList;
-
-  final FirebaseDatabase _database = FirebaseDatabase.instance;
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
-  final _textEditingController = TextEditingController();
-  StreamSubscription<Event> _onTodoAddedSubscription;
-  StreamSubscription<Event> _onTodoChangedSubscription;
-
-  Query _todoQuery;
-
   bool _isEmailVerified = false;
 
   @override
@@ -35,16 +22,6 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     _checkEmailVerification();
-
-    _todoList = new List();
-    _todoQuery = _database
-        .reference()
-        .child("todo")
-        .orderByChild("userId")
-        .equalTo(widget.userId);
-    _onTodoAddedSubscription = _todoQuery.onChildAdded.listen(onEntryAdded);
-    _onTodoChangedSubscription =
-        _todoQuery.onChildChanged.listen(onEntryChanged);
   }
 
   void _checkEmailVerification() async {
@@ -109,30 +86,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  @override
-  void dispose() {
-    _onTodoAddedSubscription.cancel();
-    _onTodoChangedSubscription.cancel();
-    super.dispose();
-  }
-
-  onEntryChanged(Event event) {
-    var oldEntry = _todoList.singleWhere((entry) {
-      return entry.key == event.snapshot.key;
-    });
-
-    setState(() {
-      _todoList[_todoList.indexOf(oldEntry)] =
-          Todo.fromSnapshot(event.snapshot);
-    });
-  }
-
-  onEntryAdded(Event event) {
-    setState(() {
-      _todoList.add(Todo.fromSnapshot(event.snapshot));
-    });
-  }
-
   signOut() async {
     try {
       await widget.auth.signOut();
@@ -142,129 +95,17 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  addNewTodo(String todoItem) {
-    if (todoItem.length > 0) {
-      Todo todo = new Todo(todoItem.toString(), widget.userId, false);
-      _database.reference().child("todo").push().set(todo.toJson());
-    }
-  }
-
-  updateTodo(Todo todo) {
-    //Toggle completed
-    todo.completed = !todo.completed;
-    if (todo != null) {
-      _database.reference().child("todo").child(todo.key).set(todo.toJson());
-    }
-  }
-
-  deleteTodo(String todoId, int index) {
-    _database.reference().child("todo").child(todoId).remove().then((_) {
-      print("Delete $todoId successful");
-      setState(() {
-        _todoList.removeAt(index);
-      });
-    });
-  }
-
-  showAddTodoDialog(BuildContext context) async {
-    _textEditingController.clear();
-    await showDialog<String>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: new Row(
-              children: <Widget>[
-                new Expanded(
-                    child: new TextField(
-                  controller: _textEditingController,
-                  autofocus: true,
-                  decoration: new InputDecoration(
-                    labelText: 'Add new todo',
-                  ),
-                ))
-              ],
-            ),
-            actions: <Widget>[
-              new FlatButton(
-                  child: const Text('Cancel'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              new FlatButton(
-                  child: const Text('Save'),
-                  onPressed: () {
-                    addNewTodo(_textEditingController.text.toString());
-                    Navigator.pop(context);
-                  })
-            ],
-          );
-        });
-  }
-
-  Widget showTodoList() {
-    if (_todoList.length > 0) {
-      return ListView.builder(
-          shrinkWrap: true,
-          itemCount: _todoList.length,
-          itemBuilder: (BuildContext context, int index) {
-            String todoId = _todoList[index].key;
-            String subject = _todoList[index].subject;
-            bool completed = _todoList[index].completed;
-            String userId = _todoList[index].userId;
-            return Dismissible(
-              key: Key(todoId),
-              background: Container(color: Colors.red),
-              onDismissed: (direction) async {
-                deleteTodo(todoId, index);
-              },
-              child: ListTile(
-                title: Text(
-                  subject,
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                trailing: IconButton(
-                    icon: (completed)
-                        ? Icon(
-                            Icons.done_outline,
-                            color: Colors.green,
-                            size: 20.0,
-                          )
-                        : Icon(Icons.done, color: Colors.grey, size: 20.0),
-                    onPressed: () {
-                      updateTodo(_todoList[index]);
-                    }),
-              ),
-            );
-          });
-    } else {
-      return Center(
-          child: Text(
-        "Welcome. Your list is empty",
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 30.0),
-      ));
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text('Hoopr'),
-          actions: <Widget>[
-            new FlatButton(
-                child: new Text('Logout',
-                    style: new TextStyle(fontSize: 17.0, color: Colors.white)),
-                onPressed: signOut)
-          ],
-        ),
-        body: showTodoList(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showAddTodoDialog(context);
-          },
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ));
+      title: new Text('Hoopr'),
+      actions: <Widget>[
+        new FlatButton(
+            child: new Text('Logout',
+                style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+            onPressed: signOut)
+      ],
+    ));
   }
 }
--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -17,51 +17,51 @@ abstract class BaseAuth {
   Future<bool> isEmailVerified();
 }
 
-class Auth implements BaseAuth {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+// class Auth implements BaseAuth {
+// final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  @override
-  Future<String> signIn(String email, String password) async {
-    AuthResult result = await _firebaseAuth.signInWithEmailAndPassword(
-        email: email, password: password);
-    FirebaseUser user = result.user;
-    if (user.isEmailVerified) return user.uid;
-    return null;
-  }
+// @override
+// Future<String> signIn(String email, String password) async {
+//   AuthResult result = await _firebaseAuth.signInWithEmailAndPassword(
+//       email: email, password: password);
+//   FirebaseUser user = result.user;
+//   if (user.isEmailVerified) return user.uid;
+//   return null;
+// }
 
-  @override
-  Future<String> signUp(String email, String password) async {
-    AuthResult result = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
-    FirebaseUser user = result.user;
-    return user.uid;
-  }
+// @override
+// Future<String> signUp(String email, String password) async {
+//   AuthResult result = await _firebaseAuth.createUserWithEmailAndPassword(
+//       email: email, password: password);
+//   FirebaseUser user = result.user;
+//   return user.uid;
+// }
 
-  @override
-  Future<FirebaseUser> getCurrentUser() async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    return user;
-  }
+// @override
+// Future<FirebaseUser> getCurrentUser() async {
+//   FirebaseUser user = await _firebaseAuth.currentUser();
+//   return user;
+// }
 
-  @override
-  Future<void> signOut() async {
-    return _firebaseAuth.signOut();
-  }
+// @override
+// Future<void> signOut() async {
+//   return _firebaseAuth.signOut();
+// }
 
-  @override
-  Future<void> sendEmailVerification() async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    user.sendEmailVerification();
-  }
+// @override
+// Future<void> sendEmailVerification() async {
+//   FirebaseUser user = await _firebaseAuth.currentUser();
+//   user.sendEmailVerification();
+// }
 
-  @override
-  Future<void> sendPasswordReset(String email) async {
-    await _firebaseAuth.sendPasswordResetEmail(email: email);
-  }
+// @override
+// Future<void> sendPasswordReset(String email) async {
+//   await _firebaseAuth.sendPasswordResetEmail(email: email);
+// }
 
-  @override
-  Future<bool> isEmailVerified() async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    return user != null && user.isEmailVerified;
-  }
-}
+// @override
+// Future<bool> isEmailVerified() async {
+//   FirebaseUser user = await _firebaseAuth.currentUser();
+//   return user != null && user.isEmailVerified;
+// }
+// }
