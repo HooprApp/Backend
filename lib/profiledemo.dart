710e568388003bb6910362207cf0c469aa6272e7
Hok Laam Cheng <hoklaam.cheng@gmail.com>
2020-09-28 23:43:03 -0400

    Logout button (#7)
    * Add logout button
    
    * Update gitignore
    
    * Delete .last_build_id

--- lib/profiledemo.dart
+++ lib/profiledemo.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:toggle_switch/toggle_switch.dart';
+import 'package:hoopr/services/authentication.dart';
 
 bool badgeView = true;
 
 class ProfileDemo extends StatefulWidget {
+  ProfileDemo({this.auth, this.userId, this.logoutCallback});
+
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
   @override
   _ProfileDemoState createState() => _ProfileDemoState();
 }
@@ -12,7 +19,15 @@ class _ProfileDemoState extends State<ProfileDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile 1')),
+      appBar: AppBar(
+        title: Text('Profile 1'),
+        actions: <Widget>[
+          new FlatButton(
+              child: new Text('Logout',
+                  style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+              onPressed: signOut)
+        ],
+      ),
       backgroundColor: const Color(0xff001331),
       body: Column(
         children: [
@@ -49,8 +64,7 @@ class _ProfileDemoState extends State<ProfileDemo> {
                           Text("@RayTran",
                               style: TextStyle(
                                 color: Colors.white,
-                              )
-                          ),
+                              )),
                         ],
                       ),
                     )
@@ -69,7 +83,7 @@ class _ProfileDemoState extends State<ProfileDemo> {
             padding: EdgeInsets.all(10.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [ 
+              children: [
                 ToggleSwitch(
                   initialLabelIndex: 0,
                   minWidth: 90.0,
@@ -88,30 +102,39 @@ class _ProfileDemoState extends State<ProfileDemo> {
       ),
     );
   }
-}
 
-Widget _badgeHistoryView() {
-  if (badgeView) {
-    return Container(
-        height: 255,
-        width: 350,
-        decoration: new BoxDecoration(
-          border: Border.all(width: 2.0, color: const Color(0xff001331)),
-          borderRadius: new BorderRadius.circular(20.0),
-        ),
-        child: Center(
-            child: Text('No badges to show.',
-                style: TextStyle(color: Colors.white))));
-  } else {
-    return Container(
-        height: 255,
-        width: 350,
-        decoration: new BoxDecoration(
-          border: Border.all(width: 2.0, color: const Color(0xff001331)),
-          borderRadius: new BorderRadius.circular(20.0),
-        ),
-        child: Center(
-            child: Text('No matches to show.',
-                style: TextStyle(color: Colors.white))));
+  signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.logoutCallback();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Widget _badgeHistoryView() {
+    if (badgeView) {
+      return Container(
+          height: 255,
+          width: 350,
+          decoration: new BoxDecoration(
+            border: Border.all(width: 2.0, color: const Color(0xff001331)),
+            borderRadius: new BorderRadius.circular(20.0),
+          ),
+          child: Center(
+              child: Text('No badges to show.',
+                  style: TextStyle(color: Colors.white))));
+    } else {
+      return Container(
+          height: 255,
+          width: 350,
+          decoration: new BoxDecoration(
+            border: Border.all(width: 2.0, color: const Color(0xff001331)),
+            borderRadius: new BorderRadius.circular(20.0),
+          ),
+          child: Center(
+              child: Text('No matches to show.',
+                  style: TextStyle(color: Colors.white))));
+    }
   }
 }
