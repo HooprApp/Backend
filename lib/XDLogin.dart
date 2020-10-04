0fce3c20312da5ffa04198f28e70689fcba73906
Edmund Lui <48935039+Edmund-Lui98@users.noreply.github.com>
2020-10-03 20:54:10 -0400

    updated sign up db data (#9)

--- lib/XDLogin.dart
+++ lib/XDLogin.dart
@@ -57,7 +57,6 @@ class _XDLogin extends State<XDLogin> {
             return;
           }
         } else if (_formMode == FormMode.SIGNUP) {
-          print("hello");
           userId = await widget.auth.signUp(_email, _password);
           print(userId);
           widget.auth.sendEmailVerification();
@@ -350,7 +349,7 @@ class _XDLogin extends State<XDLogin> {
         ),
         validator: (value) =>
             value.isEmpty ? 'First name can\'t be empty' : null,
-        onSaved: (value) => _email = value.trim(),
+        onSaved: (value) => _firstname = value.trim(),
       ),
     );
   }
@@ -387,7 +386,7 @@ class _XDLogin extends State<XDLogin> {
         ),
         validator: (value) =>
             value.isEmpty ? 'Last name can\'t be empty' : null,
-        onSaved: (value) => _email = value.trim(),
+        onSaved: (value) => _lastname = value.trim(),
       ),
     );
   }
@@ -423,7 +422,7 @@ class _XDLogin extends State<XDLogin> {
           ),
         ),
         validator: (value) => value.isEmpty ? 'Username can\'t be empty' : null,
-        onSaved: (value) => _email = value.trim(),
+        onSaved: (value) => _username = value.trim(),
       ),
     );
   }
--- lib/services/user.dart
+++ lib/services/user.dart
@@ -1,17 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-
   final String uid;
   DatabaseService({this.uid});
 
   //collection reference
-  final CollectionReference userCollection = Firestore.instance.collection('users');
+  final CollectionReference userCollection =
+      Firestore.instance.collection('users');
 
-  Future updateUserData(String name, int age) async {
+  Future updateUserData(
+      String firstName, String lastName, String username, int bp) async {
     return await userCollection.document(uid).setData({
-      'name': name,
-      'age':age,
+      'firstName': firstName,
+      'lastName': lastName,
+      'username': username,
+      'bp': bp
     });
   }
 }
