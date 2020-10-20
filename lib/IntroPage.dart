05f69b257e16ca7d05f30062ab69d6b64657cb2d
a6you <austiny365@gmail.com>
2020-10-19 21:27:38 -0400

    Fix UI for Welcome screen

--- lib/IntroPage.dart
+++ lib/IntroPage.dart
@@ -13,63 +13,76 @@ class _IntroPagetState extends State<IntroPage> {
     return Scaffold(
         backgroundColor: const Color(0xff001331),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
                 alignment: Alignment.center,
-                padding: EdgeInsets.fromLTRB(20, 80.0, 20, 80.0),
+                padding: EdgeInsets.fromLTRB(20, 120.0, 20, 80.0),
                 child: CircleAvatar(
-                  radius: 30,
+                  radius: 90,
                   backgroundImage: AssetImage('assets/raysmall.png'),
                 )),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("WELCOME TO HOOPR",
-                    style: TextStyle(
-                        fontFamily: 'Open Sans',
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30.0)),
-                Text("Meet your ball community.",
-                    style: TextStyle(
-                        fontFamily: 'Open Sans',
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0)),
-              ],
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+              child: Text("Welcome to Hoopr!",
+                  style: TextStyle(
+                      fontFamily: 'Open Sans',
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0)),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+              child: Text("Meet your ball community.",
+                  style: TextStyle(
+                      fontFamily: 'Open Sans',
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0)),
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  color: Colors.orange,
-                  child: Text("CREATE ACCOUNT",
-                      style: TextStyle(
-                          fontFamily: 'Open Sans',
-                          color: const Color(0xff001331),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30.0)),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => new RootPage(auth: new Auth())),
-                    );
-                  },
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 350,
+                    height: 60,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      color: Colors.orange[600],
+                      child: Text("CREATE ACCOUNT",
+                          style: TextStyle(
+                              fontFamily: 'Open Sans',
+                              color: const Color(0xff001331),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  new RootPage(auth: new Auth())),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-                FlatButton(
-                  child: Text("Already have an account?",
-                      style: TextStyle(
-                          fontFamily: 'Open Sans',
-                          color: Colors.orange,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15.0)),
-                  onPressed: () {},
-                )
+                Container(
+                    alignment: Alignment.center,
+                    child: FlatButton(
+                      child: Text("Already have an account?",
+                          style: TextStyle(
+                              fontFamily: 'Open Sans',
+                              color: Colors.orange[600],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.0)),
+                      onPressed: () {},
+                    ))
               ],
-            )
+            ),
           ],
         ));
   }
