7503f521653333da947310f6df565a618dced4df
hoklaamc <hoklaam.cheng@gmail.com>
2020-11-01 20:36:45 -0500

    Cleanup

--- lib/pages/profile/profileChallengeCard.dart
+++ lib/pages/profile/profileChallengeCard.dart
@@ -2,9 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ProfileChallengesCard extends StatefulWidget {
-  const ProfileChallengesCard(this.challengeId);
+  const ProfileChallengesCard(this.challengeIds);
 
-  final List<dynamic> challengeId;
+  final List<dynamic> challengeIds;
 
   @override
   _ProfileChallengesCardState createState() => _ProfileChallengesCardState();
@@ -17,10 +17,10 @@ class _ProfileChallengesCardState extends State<ProfileChallengesCard> {
         Firestore.instance.collection('challenges');
 
     return ListView.builder(
-        itemCount: widget.challengeId.length,
+        itemCount: widget.challengeIds.length,
         itemBuilder: (context, index) {
           return FutureBuilder<DocumentSnapshot>(
-              future: challenges.document(widget.challengeId[index]).get(),
+              future: challenges.document(widget.challengeIds[index]).get(),
               builder: (BuildContext context,
                   AsyncSnapshot<DocumentSnapshot> snapshot) {
                 if (snapshot.hasError) {
@@ -29,8 +29,6 @@ class _ProfileChallengesCardState extends State<ProfileChallengesCard> {
 
                 if (snapshot.connectionState == ConnectionState.done) {
                   Map<String, dynamic> data = snapshot.data.data;
-                  print(widget.challengeId);
-                  print(data.toString());
                   return Container(child: Text(data['receiverId']));
                 } else {
                   return Container();
