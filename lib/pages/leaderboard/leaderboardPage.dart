efdf1bd925ac78568e7848fc4b6f803ea38c248a
a6you <austiny365@gmail.com>
2020-12-24 15:56:37 -0500

    Changed podium ranking icons

--- lib/pages/leaderboard/leaderboardPage.dart
+++ lib/pages/leaderboard/leaderboardPage.dart
@@ -23,33 +23,45 @@ class LeaderboardPage extends StatelessWidget {
             padding: EdgeInsets.all(20),
             child: Column(
               children: <Widget>[
-                Expanded(
-                    flex: 1,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "LEADERBOARD",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'Open Sans',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30.0),
-                            textAlign: TextAlign.center,
-                          )
-                        ])),
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      PodiumSlot(position: 2),
-                      PodiumSlot(position: 1),
-                      PodiumSlot(position: 3)
-                    ],
+                Column(children: [
+                  Text(
+                    "LEADERBOARD",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0),
+                    textAlign: TextAlign.center,
                   ),
+                ]),
+                //Expanded(
+                //flex: 1,
+                /*child:*/ Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Column(
+                      children: [
+                        Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20),
+                            color: const Color(0xff001331)),
+                        PodiumSlot(position: 2),
+                      ],
+                    ),
+                    PodiumSlot(position: 1),
+                    Column(
+                      children: [
+                        Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20),
+                            color: const Color(0xff001331)),
+                        PodiumSlot(position: 3),
+                      ],
+                    ),
+                  ],
                 ),
+                //),
                 Expanded(flex: 4, child: LeaderboardList()),
                 Align(
                     alignment: Alignment.bottomCenter,
--- lib/pages/leaderboard/podiumSlot.dart
+++ lib/pages/leaderboard/podiumSlot.dart
@@ -5,13 +5,31 @@ class PodiumSlot extends StatelessWidget {
 
   final int position;
 
-  Icon _getIcon() {
+  Column _getIcon() {
     if (position == 1) {
-      return Icon(Icons.looks_one);
+      return Column(children: [
+        Image(
+          image: AssetImage('assets/firstplace.png'),
+          height: 30,
+          width: 30,
+        ),
+      ]);
     } else if (position == 2) {
-      return Icon(Icons.looks_two);
+      return Column(children: [
+        Image(
+          image: AssetImage('assets/secondplace.png'),
+          height: 30,
+          width: 30,
+        )
+      ]);
     } else {
-      return Icon(Icons.looks_3);
+      return Column(children: [
+        Image(
+          image: AssetImage('assets/thirdplace.png'),
+          height: 30,
+          width: 30,
+        )
+      ]);
     }
   }
 
@@ -22,7 +40,7 @@ class PodiumSlot extends StatelessWidget {
         alignment: Alignment.bottomCenter,
         children: [
           CircleAvatar(
-            radius: 30,
+            radius: 40,
             backgroundImage: AssetImage('assets/bballplayer2.jpeg'),
           ),
           _getIcon(),
